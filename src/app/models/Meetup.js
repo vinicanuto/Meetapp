c9ca35f111c081e84a766956a8ec790c611fb9cd
Vinicius Canuto <vcanutodev@gmail.com>
2019-09-12 18:47:31 -0300

    gerenciamento de arquivos, model meetup

---
+++ src/app/models/Meetup.js
@@ -0,0 +1,25 @@
+import Sequelize, { Model } from 'sequelize';
+import { isBefore } from 'date-fns';
+
+class Meetup extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        title: Sequelize.STRING,
+        description: Sequelize.STRING,
+        location: Sequelize.STRING,
+        date: Sequelize.DATE,
+        past: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(this.date, new Date());
+          },
+        },
+      },
+      { sequelize }
+    );
+    return this;
+  }
+}
+
+export default new Meetup();
